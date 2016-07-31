1375d4d89b99d527dd8c1b471ad00ebc05a18b03
Enrique Urbina <btace1347@yahoo.com>
2016-07-31 02:33:39 -0500

    added pictures and logos

--- HP.js
+++ HP.js
@@ -1,1 +1,16 @@
 
+
+    var myString = "WELCOME ACES";
+    var myArray = myString.split("");
+    var loopTimer;
+
+    function frameLooper() {
+        if (myArray.length > 0) {
+            document.getElementById("myTypingText").innerHTML += myArray.shift();
+        } else {
+            clearTimeout(loopTimer);
+            return false;
+        }
+        loopTimer = setTimeout('frameLooper()', 100);
+    }
+    frameLooper();
